e9357916e8251ae736d8737a7f2f5baaf164e78a
hajungkim <haha950903@naver.com>
2021-10-21 11:20:16 +0900

    [BE][Fix] 스웨거 수정

--- backend/src/main/java/com/ssafy/profolio/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/profolio/config/SwaggerConfig.java
@@ -2,19 +2,36 @@ package com.ssafy.profolio.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+    // swagger 접속 URL
+    // localhost:8080/swagger-ui.html
+
     @Bean
-    public Docket api() {
+    public Docket postsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("proFolio")
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ssafy.profolio.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ssafy.profolio.web"))
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("proFolio")
+                .description("프로폴리오 API 설명서")
+                .build();
+    }
+
 }
