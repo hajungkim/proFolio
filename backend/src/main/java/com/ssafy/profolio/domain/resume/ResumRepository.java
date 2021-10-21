6d30231907b063e5526f3348b1501a1dc0abf40b
hyeyemin <wlalsdns@naver.com>
2021-10-21 11:40:04 +0900

    [BE][Fix] entity cascade 추가

--- backend/src/main/java/com/ssafy/profolio/domain/resume/ResumRepository.java
+++ backend/src/main/java/com/ssafy/profolio/domain/resume/ResumRepository.java
@@ -3,4 +3,6 @@ package com.ssafy.profolio.domain.resume;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResumRepository extends JpaRepository<Resume, Long> {
+    void delete(Resume resume);
+    Resume getById(Long id);
 }
--- backend/src/main/java/com/ssafy/profolio/domain/resume/Resume.java
+++ backend/src/main/java/com/ssafy/profolio/domain/resume/Resume.java
@@ -1,10 +1,19 @@
 package com.ssafy.profolio.domain.resume;
 
+import com.ssafy.profolio.domain.activity.Activity;
+import com.ssafy.profolio.domain.awards.Awards;
+import com.ssafy.profolio.domain.career.Career;
+import com.ssafy.profolio.domain.certificate.Certificate;
+import com.ssafy.profolio.domain.foreignlang.Foreignlang;
+import com.ssafy.profolio.domain.project.Project;
+import com.ssafy.profolio.domain.technologystack.TechnologyStack;
 import com.ssafy.profolio.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,6 +27,27 @@ public class Resume {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<TechnologyStack> technologyStackList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Activity> activityList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Awards> awardsList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Career> careerList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Certificate> certificateList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Foreignlang> foreignlangList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Project> projectList = new ArrayList<>();
 
     public Resume() {}
 
