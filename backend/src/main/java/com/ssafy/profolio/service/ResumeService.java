5f5f8e1d25cb09a0a5df070a3e65479dfa3e3496
hajungkim <haha950903@naver.com>
2021-10-21 11:18:20 +0900

    [BE][Feat] 서비스, 컨트롤러 작업중

--- backend/src/main/java/com/ssafy/profolio/service/ResumeService.java
+++ backend/src/main/java/com/ssafy/profolio/service/ResumeService.java
@@ -1,4 +1,38 @@
 package com.ssafy.profolio.service;
 
+import com.ssafy.profolio.domain.activity.ActivityRepository;
+import com.ssafy.profolio.domain.awards.AwardsRepository;
+import com.ssafy.profolio.domain.career.CareerRepository;
+import com.ssafy.profolio.domain.certificate.CertificateRepository;
+import com.ssafy.profolio.domain.education.EducationRepository;
+import com.ssafy.profolio.domain.foreignlang.ForeignlangRepository;
+import com.ssafy.profolio.domain.project.ProjectRepository;
+import com.ssafy.profolio.domain.resume.ResumRepository;
+import com.ssafy.profolio.domain.resume.Resume;
+import com.ssafy.profolio.domain.technologystack.TechnologyStackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ResumeService {
+
+    private final ResumRepository resumRepository;
+    private final ActivityRepository activityRepository;
+    private final AwardsRepository awardsRepository;
+    private final CareerRepository careerRepository;
+    private final CertificateRepository certificateRepository;
+    private final EducationRepository educationRepository;
+    private final ForeignlangRepository foreignlangRepository;
+    private final ProjectRepository projectRepository;
+    private final TechnologyStackRepository technologyStackRepository;
+
+//    @Transactional
+//    public void saveResume(Long userId) {
+//        Resume resume = Resume.builder()
+//                .user(userId)
+//                .build();
+//    }
+
 }
--- backend/src/main/java/com/ssafy/profolio/web/ResumeController.java
+++ backend/src/main/java/com/ssafy/profolio/web/ResumeController.java
@@ -1,4 +1,19 @@
 package com.ssafy.profolio.web;
 
+import com.ssafy.profolio.service.ResumeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RequiredArgsConstructor
+@RestController
+@RequestMapping
 public class ResumeController {
+
+    private final ResumeService resumeService;
+
+
+
 }
