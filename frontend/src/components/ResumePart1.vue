be1bae819877e3e6058032520dcb18bb2ce48340
최정은 <wish4517@naver.com>
2021-10-25 10:37:59 +0900

    [FE][Design] resume progress bar

---
+++ frontend/src/components/ResumePart1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>ResumePart1</div>
+</template>
+
+<script>
+export default {
+  name: 'ResumePart1',
+};
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Resume.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="resume">
+    <div class="resume-wrapper">
+      <ul class="step-progress">
+        <li class="step-progress-item current" data-step="1" @click="stepChange">
+          <strong>인적사항</strong>
+        </li>
+        <li class="step-progress-item" data-step="2" @click="stepChange">
+          <strong>학력/경력/활동</strong>
+        </li>
+        <li class="step-progress-item" data-step="3" @click="stepChange">
+          <strong>어학/자격증/수상</strong>
+        </li>
+        <li class="step-progress-item" data-step="4" @click="stepChange">
+          <strong>기술스택</strong>
+        </li>
+        <li class="step-progress-item" data-step="5" @click="stepChange">
+          <strong>프로젝트</strong>
+        </li>
+      </ul>
+    </div>
+    <div class="resume-part"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resume',
+  methods: {
+    stepChange(event) {
+      const Now = event.target.getAttribute('data-step') - 1;
+      const stepList = document.querySelectorAll('.step-progress-item');
+      for (let i = 0; i < 5; i += 1) {
+        if (i < Now) stepList[i].className = 'step-progress-item is-done';
+        else if (i === Now) stepList[i].className = 'step-progress-item current';
+        else stepList[i].className = 'step-progress-item';
+      }
+    },
+  },
+};
+</script>
+
+<style>
+@import '../assets/styles/Resume.css';
+</style>
