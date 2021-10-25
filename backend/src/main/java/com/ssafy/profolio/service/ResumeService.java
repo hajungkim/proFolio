f4435159bb02d19001e4b86b015818b9c10f0a05
hajungkim <haha950903@naver.com>
2021-10-25 16:22:13 +0900

    [BE][Feat] 유저이력서 read api

--- backend/src/main/java/com/ssafy/profolio/service/ResumeService.java
+++ backend/src/main/java/com/ssafy/profolio/service/ResumeService.java
@@ -18,9 +18,15 @@ import com.ssafy.profolio.domain.technologystack.TechnologyStack;
 import com.ssafy.profolio.domain.technologystack.TechnologyStackRepository;
 import com.ssafy.profolio.domain.user.User;
 import com.ssafy.profolio.domain.user.UserRepository;
+import com.ssafy.profolio.exception.BaseException;
+import com.ssafy.profolio.web.BaseResponseCode;
+import com.ssafy.profolio.web.dto.ResumeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +42,90 @@ public class ResumeService {
     private final ProjectRepository projectRepository;
     private final TechnologyStackRepository technologyStackRepository;
 
+    @Transactional(readOnly = true)
+    public List<ResumeDto.ActivityResponse> findActivity(Long userId) {
+        List<Activity> activityList = activityRepository.findByUserId(userId);
+        List<ResumeDto.ActivityResponse> result = new ArrayList<>();
+        for (Activity activity : activityList) {
+            result.add(new ResumeDto.ActivityResponse(activity.getId(), activity.getName(), activity.getDescription(),
+                    activity.getStartDate(), activity.getEndDate(), activity.getOrganization()));
+        }
+        return result;
+    }
 
+    @Transactional(readOnly = true)
+    public List<ResumeDto.AwardsResponse> findAwards(Long userId) {
+        List<Awards> awardsList = awardsRepository.findByUserId(userId);
+        List<ResumeDto.AwardsResponse> result = new ArrayList<>();
+        for (Awards awards : awardsList) {
+            result.add(new ResumeDto.AwardsResponse(awards.getId(), awards.getName(), awards.getPrize(),
+                    awards.getDescription(), awards.getAwardsDate()));
+        }
+        return result;
+    }
 
+    @Transactional(readOnly = true)
+    public List<ResumeDto.CareerResponse> findCareer(Long userId) {
+        List<Career> careerList = careerRepository.findByUserId(userId);
+        List<ResumeDto.CareerResponse> result = new ArrayList<>();
+        for (Career career : careerList) {
+            result.add(new ResumeDto.CareerResponse(career.getId(), career.getCompany(), career.getDuty(),
+                    career.getStartDate(), career.getEndDate(), career.getDescription()));
+        }
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ResumeDto.CertificateResponse> findCertificate(Long userId) {
+        List<Certificate> certificateList = certificateRepository.findByUserId(userId);
+        List<ResumeDto.CertificateResponse> result = new ArrayList<>();
+        for (Certificate certificate : certificateList) {
+            result.add(new ResumeDto.CertificateResponse(certificate.getId(), certificate.getName(),
+                    certificate.getOrganization(), certificate.getCertifiedDate()));
+        }
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ResumeDto.ForeignLangResponse> findForeignLang(Long userId) {
+        List<Foreignlang> foreignlangList = foreignlangRepository.findByUserId(userId);
+        List<ResumeDto.ForeignLangResponse> result = new ArrayList<>();
+        for (Foreignlang foreignlang : foreignlangList) {
+            result.add(new ResumeDto.ForeignLangResponse(foreignlang.getId(), foreignlang.getLanguage(),
+                    foreignlang.getName(), foreignlang.getScore(), foreignlang.getCertifiedDate()));
+        }
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ResumeDto.ProjectResponse> findProject(Long userId) {
+        List<Project> projectList = projectRepository.findByUserId(userId);
+        List<ResumeDto.ProjectResponse> result = new ArrayList<>();
+        for (Project project : projectList) {
+            result.add(new ResumeDto.ProjectResponse(project.getId(), project.getTitle(), project.getSummary(),
+                    project.getDescription(), project.getMemberCnt(), project.getTechnologyStack(), project.getRole(),
+                    project.getLink(), project.getStartDate(), project.getEndDate()));
+        }
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ResumeDto.TechnologyStackResponse> findTechnologyStack(Long userId) {
+        List<TechnologyStack> technologyStackList = technologyStackRepository.findByUserId(userId);
+        List<ResumeDto.TechnologyStackResponse> result = new ArrayList<>();
+        for (TechnologyStack technologyStack : technologyStackList) {
+            result.add(new ResumeDto.TechnologyStackResponse(technologyStack.getId(), technologyStack.getName(), technologyStack.getLevel(),
+                    technologyStack.getDescription(), technologyStack.getKind()));
+        }
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public ResumeDto.EducationResponse findEducation(Long userId) {
+        Education education = educationRepository.findByUserId(userId);
+        if (education == null) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
+        return new ResumeDto.EducationResponse(education.getId(), education.getUniversity(), education.isMainSchool(),
+                education.getMajor(), education.getMinor(), education.getAdmissionDate(), education.getGraduationDate(),
+                education.isGraduation(), education.getScore(), education.getTotalScore());
+    }
 }
