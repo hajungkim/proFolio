04127891ca8b56b85efb2ccea9cdfdad18d76856
hajungkim <haha950903@naver.com>
2021-10-25 16:20:13 +0900

    [BE][Feat] 유저id로 find

--- backend/src/main/java/com/ssafy/profolio/domain/education/EducationRepository.java
+++ backend/src/main/java/com/ssafy/profolio/domain/education/EducationRepository.java
@@ -3,4 +3,6 @@ package com.ssafy.profolio.domain.education;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EducationRepository extends JpaRepository<Education, Long> {
+
+    Education findByUserId(Long userId);
 }
