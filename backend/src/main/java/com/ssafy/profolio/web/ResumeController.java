7f88f478db86d176d1524a2704a8e42e3e2317cf
hajungkim <haha950903@naver.com>
2021-10-26 10:56:53 +0900

    [BE][Feat] 이력서 save

--- backend/src/main/java/com/ssafy/profolio/web/ResumeController.java
+++ backend/src/main/java/com/ssafy/profolio/web/ResumeController.java
@@ -16,59 +16,107 @@ public class ResumeController {
 
     private final ResumeService resumeService;
 
-    @GetMapping("/activity/{id}")
-    public BaseResponse<List<ResumeDto.ActivityResponse>> findActivity(@PathVariable("id") Long userId) {
+    @GetMapping("/activity/{userid}")
+    public BaseResponse<List<ResumeDto.ActivityResponse>> findActivity(@PathVariable("userid") Long userId) {
         List<ResumeDto.ActivityResponse> responseList = resumeService.findActivity(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/awards/{id}")
-    public BaseResponse<List<ResumeDto.AwardsResponse>> findAwards(@PathVariable("id") Long userId) {
+    @GetMapping("/awards/{userid}")
+    public BaseResponse<List<ResumeDto.AwardsResponse>> findAwards(@PathVariable("userid") Long userId) {
         List<ResumeDto.AwardsResponse> responseList = resumeService.findAwards(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/career/{id}")
-    public BaseResponse<List<ResumeDto.CareerResponse>> findCareer(@PathVariable("id") Long userId) {
+    @GetMapping("/career/{userid}")
+    public BaseResponse<List<ResumeDto.CareerResponse>> findCareer(@PathVariable("userid") Long userId) {
         List<ResumeDto.CareerResponse> responseList = resumeService.findCareer(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/certificate/{id}")
-    public BaseResponse<List<ResumeDto.CertificateResponse>> findCertificate(@PathVariable("id") Long userId) {
+    @GetMapping("/certificate/{userid}")
+    public BaseResponse<List<ResumeDto.CertificateResponse>> findCertificate(@PathVariable("userid") Long userId) {
         List<ResumeDto.CertificateResponse> responseList = resumeService.findCertificate(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/foreign/{id}")
-    public BaseResponse<List<ResumeDto.ForeignLangResponse>> findForeignLang(@PathVariable("id") Long userId) {
+    @GetMapping("/foreign/{userid}")
+    public BaseResponse<List<ResumeDto.ForeignLangResponse>> findForeignLang(@PathVariable("userid") Long userId) {
         List<ResumeDto.ForeignLangResponse> responseList = resumeService.findForeignLang(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/project/{id}")
-    public BaseResponse<List<ResumeDto.ProjectResponse>> findProject(@PathVariable("id") Long userId) {
+    @GetMapping("/project/{userid}")
+    public BaseResponse<List<ResumeDto.ProjectResponse>> findProject(@PathVariable("userid") Long userId) {
         List<ResumeDto.ProjectResponse> responseList = resumeService.findProject(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/technology/{id}")
-    public BaseResponse<List<ResumeDto.TechnologyStackResponse>> findTechnologyStack(@PathVariable("id") Long userId) {
+    @GetMapping("/technology/{userid}")
+    public BaseResponse<List<ResumeDto.TechnologyStackResponse>> findTechnologyStack(@PathVariable("userid") Long userId) {
         List<ResumeDto.TechnologyStackResponse> responseList = resumeService.findTechnologyStack(userId);
         if (responseList.size() == 0) throw new BaseException(BaseResponseCode.DATA_IS_NULL);
         return new BaseResponse<>(responseList);
     }
 
-    @GetMapping("/education/{id}")
-    public BaseResponse<ResumeDto.EducationResponse> findEducation(@PathVariable("id") Long userId) {
+    @GetMapping("/education/{userid}")
+    public BaseResponse<ResumeDto.EducationResponse> findEducation(@PathVariable("userid") Long userId) {
         ResumeDto.EducationResponse response = resumeService.findEducation(userId);
         return new BaseResponse<>(response);
+    }
+
+    @PostMapping("/activity")
+    public BaseResponse<String> saveActivity(@RequestBody ResumeDto.ActivityRequest request) {
+        resumeService.saveActivity(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/awards")
+    public BaseResponse<String> saveAwards(@RequestBody ResumeDto.AwardsRequest request) {
+        resumeService.saveAwards(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/career")
+    public BaseResponse<String> saveCareer(@RequestBody ResumeDto.CareerRequest request) {
+        resumeService.saveCareer(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/certificate")
+    public BaseResponse<String> saveCertificate(@RequestBody ResumeDto.CertificateRequest request) {
+        resumeService.saveCertificate(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/education")
+    public BaseResponse<String> saveEducation(@RequestBody ResumeDto.EducationRequest request) {
+        resumeService.saveEducation(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/foreign")
+    public BaseResponse<String> saveForeignLang(@RequestBody ResumeDto.ForeignLangRequest request) {
+        resumeService.saveForeignLang(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/project")
+    public BaseResponse<String> saveProject(@RequestBody ResumeDto.ProjectRequest request) {
+        resumeService.saveProject(request);
+        return new BaseResponse<>("저장 완료");
+    }
+
+    @PostMapping("/technology")
+    public BaseResponse<String> saveTechnologyStack(@RequestBody ResumeDto.TechnologyStackRequest request) {
+        resumeService.saveTechnologyStack(request);
+        return new BaseResponse<>("저장 완료");
     }
 
 }
