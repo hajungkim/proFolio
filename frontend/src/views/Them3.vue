60ad985d1d0cf2b66fb05b944d5b69c92a7b0571
hyeyemin <wlalsdns@naver.com>
2021-10-28 13:18:12 +0900

    [FE][Feat]템플릿3 Info

--- frontend/src/views/Them3.vue
+++ frontend/src/views/Them3.vue
@@ -1,21 +1,35 @@
 <template>
-  <div>
+  <div class="them3">
+      <Them3Info/>
+      <div class="them3-content">
+        <div class="menu">
+          <ul class="itmes">
+            <li>Info</li>
+            <li>Education</li>
+            <li>Certificate</li>
+            <li>Experience</li>
+            <li>Skills</li>
+            <li>Awards</li>
+            <li>Projects</li>
+          </ul>
+          <div class="menu-buttons">
+            <div>저장</div>
+            <div>변환</div>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import Them3Info from '../components/Them3Info.vue';
 
 export default {
+  components: { Them3Info },
   name: 'Them3',
   data() {
     return {
     };
-  },
-  computed: {
-    ...mapState([
-      'resume',
-    ]),
   },
 };
 </script>
