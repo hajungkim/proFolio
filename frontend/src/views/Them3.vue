f49125475cc413cb471f80ee509c74e4704a5d13
Donggun Ko <dongjuk157@gmail.com>
2021-11-05 17:04:34 +0900

    [FE][Design] Modify menu's background in them3

--- frontend/src/views/Them3.vue
+++ frontend/src/views/Them3.vue
@@ -18,11 +18,11 @@
             </svg>
           </div>
           <h4>complete</h4>
-          <div class="buttons">
+          <div class="menu-buttons">
             <button class="btn-hover color-9">저장하기</button>
             <button class="btn-hover color-9" @click="openModal">PDF변환</button>
             <!-- edit -->
-            <button id="edit-btn" @click="clickEdit">{{editBtn}}</button>
+            <button id="edit-btn" @click="clickEdit" class="btn-hover">{{editBtn}}</button>
           </div>
       </div>
       <div class="them3-content">
