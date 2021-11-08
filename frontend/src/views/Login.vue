ea0509b0832cc187e545eb9981f2e78a9f889069
lee.sujeong10111 <lee.sujeong10111@gmail.com>
2021-11-08 17:23:50 +0900

    [FE][fix] 로그인 페이지 디자인 변경

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,29 +2,31 @@
   <div class="login">
     <div class="login-box">
       <div class="login-letter">
-        <div class="font-40 font-w-800 font-padding">Login</div>
+        <img src="../assets/images/logo1.png" alt="" class="login-logo">
+        <div class="font-40 font-w-800 font-padding">로그인</div>
         <div>proFolio로</div>
         <div>쉽게 포트폴리오를 완성해보세요</div>
       </div>
+      <div class="vl"></div>
       <div class="social-outer-box">
         <div>
           <div class="social-box" @click="googleLogin">
             <div class="inline-block">
               <img src="../assets/images/google.png" alt="" class="social-logo">
             </div>
-            <div class="inline-block padding-l">Login in with Google</div>
+            <div class="inline-block padding-l">구글 계정으로 로그인</div>
           </div>
           <div class="social-box naver" @click="naverLogin">
             <div class="inline-block">
               <span class="social-logo-letter">N</span>
             </div>
-            <div class="inline-block padding-l">Login in with Naver</div>
+            <div class="inline-block padding-l">네이버 계정으로 로그인</div>
           </div>
           <div class="social-box github" @click="githubLogin">
             <div class="inline-block">
               <img src="../assets/images/github.png" alt="" class="social-logo">
             </div>
-            <div class="inline-block padding-l">Login in with Github</div>
+            <div class="inline-block padding-l">깃허브 계정으로 로그인</div>
           </div>
         </div>
       </div>
